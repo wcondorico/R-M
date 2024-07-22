e1e7cc44d0ba5b4274fd363ef2122773fbfb3c64
William <williamcondori50@gmail.com>
2024-07-22 17:41:49 -0500

    feature: localeId added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeEsPE from '@angular/common/locales/es-PE';
 
 import { routes } from './app.routes';
 
+registerLocaleData(localeEsPE, 'es-PE');
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es-PE' }
+  ]
+
+
 };
