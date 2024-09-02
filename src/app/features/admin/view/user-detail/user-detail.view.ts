e992f5aa6cb4b23b535b50d76378ca1a56d06a6c
William <williamcondori50@gmail.com>
2024-09-01 20:16:41 -0500

    refactor: user table completed

--- src/app/features/admin/view/user-detail/user-detail.view.ts
+++ src/app/features/admin/view/user-detail/user-detail.view.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './user-detail.view.html',
   styleUrl: './user-detail.view.scss'
 })
 export class UserDetailView {
+  hide = signal(true);
 
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.min(8)]);
+  nameFormControl = new FormControl('', [Validators.required]);
+  lastNameFormControl = new FormControl('', [Validators.required]);
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
--- src/app/features/admin/view/user/user.view.ts
+++ src/app/features/admin/view/user/user.view.ts
@@ -1,36 +1,46 @@
-import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { RouterModule } from '@angular/router';
 
-export interface PeriodicElement {
+export interface Data {
   name: string;
-  position: number;
-  weight: number;
+  lastName: string;
   detalle: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, detalle: 'detalle'},
-  {position: 2, name: 'Helium', weight: 4.0026, detalle: 'detalle'},
-  {position: 3, name: 'Lithium', weight: 6.941, detalle: 'detalle'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, detalle: 'detalle'},
-  {position: 5, name: 'Boron', weight: 10.811, detalle: 'detalle'},
-  {position: 6, name: 'Carbon', weight: 12.0107, detalle: 'detalle'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, detalle: 'detalle'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, detalle: 'detalle'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, detalle: 'detalle'},
-  {position: 10, name: 'Neon', weight: 20.1797, detalle: 'detalle'},
+const Data: Data[] = [
+  {name: 'Juan', lastName: 'Perez', detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'},
+  {name: 'Juan', lastName: "Perez", detalle: 'detalle'}
 ];
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, RouterModule],
+  imports: [MatTableModule, RouterModule, MatPaginatorModule],
   templateUrl: './user.view.html',
   styleUrl: './user.view.scss'
 })
-export class UserView {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'detalle'];
-  dataSource = ELEMENT_DATA;
+export class UserView implements AfterViewInit{
+  displayedColumns: string[] = ['name', 'lastName', 'detalle'];
+  dataSource = new MatTableDataSource<Data>(Data);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
